50d6f053ac1c32157049070cf276029303c0a564
vinothrao <vinothrao89@gmail.com>
2024-02-19 14:53:08 +0530

    Update az-webapp-create-py.sh

--- az-webapp-create-py.sh
+++ az-webapp-create-py.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rm -f ./deploy.zip
-zip -r ./deploy.zip . -x ./.git\* ./.vscode\* ./deploy\*
+zip -r ./deploy.zip . -x ./.git\* ./.vscode\* ./deploy\* Jenkisfile az-webapp-create-py.sh
 
 az webapp config appsettings set --resource-group jenkins-test --name jenkins-py-test --settings SCM_DO_BUILD_DURING_DEPLOYMENT=true 
 if [ ! -z "$BUILDTAG" ]; then
